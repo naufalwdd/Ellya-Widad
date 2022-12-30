053efc79aaebe58691f3356aeae3c09f0dab82e2
Naufal Widad <mnwidad23@gmail.com>
2022-12-30 07:47:23 +0700

    fix router

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<MainPage />} />
-                <Route path="/Widad-Ellya" element={<WidadEllya />} />
-                <Route path="/Fulan-Fulanah" element={<FulanFulanah />} />
+                <Route exact path="/resepsi-kami" element={<MainPage />} />
+                <Route path="/resepsi-kami/Widad-Ellya" element={<WidadEllya />} />
+                <Route path="/resepsi-kami/Fulan-Fulanah" element={<FulanFulanah />} />
             </Routes>
             <link rel="preconnect" href="https://fonts.googleapis.com"></link>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
