d0a79c2a2c34665f8cbce77d07dc9b12ff60b678
Naufal Widad <mnwidad23@gmail.com>
2023-01-13 07:49:15 +0700

    change browser router to hash router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import MainPage from './pages/MainPage/MainPage'
 import WidadEllya from './pages/WidadEllya/WidadEllya';
 import EllyaWidad from './pages/EllyaWidad/EllyaWidad';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import FulanFulanah from './pages/FulanFulanah/FulanFulanah';
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/Widad-Ellya" element={<WidadEllya />} />
@@ -15,7 +15,7 @@ function App() {
             </Routes>
             <link rel="preconnect" href="https://fonts.googleapis.com"></link>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
